6ee84d2ba89b1151ffac4cd77e074fda06664235
WillW34 <78357757+WillW34@users.noreply.github.com>
2021-06-19 11:40:34 +0100

    Plugin now returns ShortBOL correct from SBOL working over flask with a given file.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os, shutil, tempfile
 import sys
 sys.path.insert(0,'shortbol')
 import shortbol.SBOL2ShortBOL as SB2Short
+#import requests
 
 app = Flask(__name__)
 
@@ -41,15 +42,13 @@ def evaluate():
 
 @app.route("/run", methods=["POST"])
 def run():
-    
     #delete if not needed
     cwd = os.getcwd()
     
     #temporary directory to write intermediate files to
     temp_dir = tempfile.TemporaryDirectory()
-    
     data = request.get_json(force=True)
-    
+
     top_level_url = data['top_level']
     complete_sbol = data['complete_sbol']
     instance_url = data['instanceUrl']
@@ -58,27 +57,28 @@ def run():
     rdf_type = data['type']
     shallow_sbol = data['shallow_sbol']
     
-    url = complete_sbol.replace('/sbol','')
-    
+    #url = complete_sbol.replace('/sbol','')
+    cwd = os.path.join(cwd, complete_sbol)
+
     try:
+
         ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
         #read in test.html
-        file_in_name = os.path.join(cwd, url)
-        with open(file_in_name, 'r') as htmlfile:
-            result = htmlfile.read()
-
-
-        result = SB2Short.produce_shortbol(result)
-        
+        file_in_name = os.path.join(cwd, "plugintest.sbol")
+        #file_in_name = requests.get(url, allow_redirects=True)
+        #with open('sbolfile', 'wb').write(file_in_name.content):
+        with open(file_in_name, 'r') as sbolfile:
+            shortbol_library = os.path.join("shortbol", "templates")
+            result = SB2Short.produce_shortbol(sbolfile.name, shortbol_library)
         
         #write out file to temporary directory
         out_name = "Out.html"
         file_out_name = os.path.join(temp_dir.name, out_name)
         with open(file_out_name, 'w') as out_file:
             out_file.write(result)
-        
         #this file could be a zip archive or any path and file name relative to temp_dir
         download_file_name = out_name
+
         ################## END SECTION ####################################
 
         return  send_from_directory(temp_dir.name,download_file_name, 
--- testing/sbh_test.py
+++ testing/sbh_test.py
@@ -5,15 +5,21 @@ import requests, zipfile, io
 eval_url = "http://127.0.0.1:5000/evaluate"
 run_url = "http://127.0.0.1:5000/run"
 
-run_data =  "{‘complete_sbol’: ‘https://dev.synbiohub.org/public/igem/BBa_E0240/1/sbol', ‘shallow_sbol’: ‘https://dev.synbiohub.org/public/igem/BBa_E0240/1/sbolnr', ‘genbank’: ‘https://dev.synbiohub.org/public/igem/BBa_E0240/1/gb', ‘top_level’: ‘https://synbiohub.org/public/igem/BBa_E0240/1', ‘size’: 39, ‘type’: ‘Component’, ‘instanceUrl’: ‘https://dev.synbiohub.org/'}"
+data = """{"complete_sbol": "testing", 
+            "shallow_sbol": "", 
+            "genbank": "", 
+            "top_level": "", 
+            "size": 39, 
+            "type": "Component", 
+            "instanceUrl": ""}"""
 
 def test():
-    eval_response = requests.post(eval_url, '{‘type’: ‘Component’}')
-    run_response = requests.post(run_url, data=run_data)
-
+    eval_response = requests.post(eval_url, data=data)
+    run_response = requests.post(run_url, data=data)
     print(eval_response.text)
-    z = zipfile.ZipFile(io.BytesIO(run_response.content))
-    z.extractall("out")
+    print(run_response.text)
+    #z = zipfile.ZipFile(io.BytesIO(run_response.content))
+    #z.extractall("out")
 
 
 if __name__ == "__main__": test()
