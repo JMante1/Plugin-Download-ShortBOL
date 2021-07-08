a84c2c01c2a1cd811de4208e6b427867cdc7c9ca
WillW34 <78357757+WillW34@users.noreply.github.com>
2021-07-08 14:14:31 +0100

    Large changes to use Test.html

--- testing/sbh_test.py
+++ testing/sbh_test.py
@@ -10,14 +10,14 @@ run_url = "http://127.0.0.1:8080/run"
 data = """{"complete_sbol": "http://localhost:5000/public/plugintest.sbol", 
             "shallow_sbol": "", 
             "genbank": "", 
-            "top_level": "", 
+            "top_level": "http://localhost:5000/public/top_level_url", 
             "size": 39, 
             "type": "Component", 
-            "instanceUrl": ""}"""
+            "instanceUrl": "http://localhost:5000/public/instance_url"}"""
 
 def test():
     save_path = 'out'
-    file_name = "test.txt"
+    file_name = "test.html"
 
     completeName = os.path.join(save_path, file_name)
 
@@ -28,7 +28,7 @@ def test():
     print(run_response.headers)
     print(run_response.status_code)
 
-    file1 = open(completeName, "w")
+    file1 = open(completeName, 'w')
     file1.write(run_response.text)
     file1.close()
 
